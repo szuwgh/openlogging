7e3c9d0c2ea09c071c47d9b794f1233f2e0a2893
szuwgh <895340293@qq.com>
2021-07-06 00:34:42 +0800

    save code

--- pkg/temql/parse/lex.go
+++ pkg/temql/parse/lex.go
@@ -1,12 +1,17 @@
 package parse
 
-import "unicode/utf8"
+import (
+	"strings"
+	"unicode/utf8"
+)
 
 const eof = -1
 
 type Pos int
 
 type ItemType int
+
+type stateFn func(*lexer) stateFn
 
 // Item represents a token or text string returned from the scanner.
 type Item struct {
@@ -30,9 +35,10 @@ var ItemTypeStr = map[ItemType]string{
 	LEFT_BRACE:  "{",
 	RIGHT_BRACE: "}",
 	ASSIGN:      "=",
+	COMMA:       ",",
 }
 
-type lex struct {
+type lexer struct {
 	input string
 	pos   int
 	len   int
@@ -41,13 +47,13 @@ type lex struct {
 	itemp *Item
 }
 
-func newLex(input string) *lex {
-	return &lex{
+func newLex(input string) *lexer {
+	return &lexer{
 		input: input,
 	}
 }
 
-func (l *lex) next() rune {
+func (l *lexer) next() rune {
 	if int(l.pos) >= len(l.input) {
 		l.width = 0
 		return eof
@@ -59,34 +65,79 @@ func (l *lex) next() rune {
 }
 
 //获取下一个item
-func (l *lex) nextItem(itemp *Item) {
+func (l *lexer) nextItem(itemp *Item) {
 	l.itemp = itemp
-	l.lexStatements()
+	//l.lexStatements()
+}
 
+func (l *lexer) backup() {
+	l.pos -= l.width
 }
 
 //生成一个item
-func (l *lex) eject(t ItemType) {
+func (l *lexer) eject(t ItemType) {
 	*l.itemp = Item{t, l.start, l.input[l.start:l.pos]}
 	l.start = l.pos
 }
 
+func lexSpace(l *lexer) stateFn {
+	for isSpace(l.peek()) {
+		l.next()
+	}
+	l.ignore()
+	return lexStatements
+}
+
+func lexInsideBraces(l *lexer) stateFn {
+	switch r := l.next(); {
+	case isSpace(r):
+		skipSpaces(l)
+	case isAlpha(r):
+	}
+
+}
+
+func lexKeywordOrIdentifier(l *lexer) stateFn {
+	for {
+		switch r := l.next(); {
+		case isAlphaNumeric(r):
+		default:
+			l.backup()
+			word := l.input[l.start:l.pos]
+			if kw, ok := key[strings.ToLower(word)]; ok {
+				l.eject(kw)
+			} else {
+				l.eject(METRIC_IDENTIFIER)
+			}
+		}
+	}
+}
+
 //lex
-func (l *lex) lexStatements() {
+func lexStatements(l *lexer) stateFn {
 	switch r := l.next(); {
 	case r == eof:
 	case r == ',':
-	case r == ':':
 	case r == '{':
+		return lexInsideBraces(l)
+	case r == '}':
+	case r == '=':
 	case isAlpha(r):
-
+		return lexKeywordOrIdentifier(l)
 	}
-
-	return 0
+	return nil
 }
 
-func lexKeywordOrIdentifier() {
+// skipSpaces skips the spaces until a non-space is encountered.
+// func skipSpaces(l *lexer) {
+// 	for isSpace(l.peek()) {
+// 		l.next()
+// 	}
+// 	//l.ignore()
+// }
 
+func isSpace(r rune) bool {
+	return r == ' ' || r == '\t' || r == '\n' || r == '\r'
 }
 
 func isAlpha(r rune) bool {
