af035e1aba4ef45795dcd2d17c55ed9f72325f48
szuwgh <895340293@qq.com>
2022-07-18 18:30:42 +0800

    debug code

--- pkg/engine/tem/logfile/logfile.go
+++ pkg/engine/tem/logfile/logfile.go
@@ -3,6 +3,7 @@ package logfile
 type LogFile interface {
 	Write(b []byte, offset int64) (int, error)
 
-	Read(b []byte, offset int64) (int, error)
-	
+	Read(offset int64) ([]byte, error)
+
+	Sync() error
 }
