dfb93f77a697f4e4b2ac7092e6fe9093710a4f89
szuwgh <895340293@qq.com>
2022-08-02 19:23:37 +0800

    add logfile

--- pkg/engine/tem/logfile/fileio.go
+++ pkg/engine/tem/logfile/fileio.go
@@ -5,3 +5,15 @@ import "os"
 type FileIO struct {
 	fd *os.File
 }
+
+func (f *FileIO) Write(b []byte, offset int64) (int, error) {
+	return 0, nil
+}
+
+func (f *FileIO) Read(offset int64) ([]byte, error) {
+	return nil, nil
+}
+
+func (f *FileIO) Sync() error {
+	return nil
+}
--- pkg/engine/tem/logfile/mmap.go
+++ pkg/engine/tem/logfile/mmap.go
@@ -6,3 +6,15 @@ type Mmap struct {
 func NewMmap(fName string, fsize int64) {
 
 }
+
+func (m *Mmap) Write(b []byte, offset int64) (int, error) {
+	return 0, nil
+}
+
+func (m *Mmap) Read(offset int64) ([]byte, error) {
+	return nil, nil
+}
+
+func (m *Mmap) Sync() error {
+	return nil
+}
